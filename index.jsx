889d64b539051f2b586c445f2e15a817914b5a60
Sejas <antonio@sejas.es>
2018-06-15 23:52:05 +0100

    nuestro primer componente

--- index.jsx
+++ index.jsx
@@ -1,1 +1,12 @@
-//Código de nuestra app React
+'use strict'
+// 0.- Nuestro primer componente
+class Hola extends React.Component {
+    render() {
+        return <h1>¡Hola Mundo!</h1>;
+    }
+}
+// 1.- Renderizar nuestra App
+ReactDOM.render(
+    <Hola />,
+    document.getElementById("root")
+)
